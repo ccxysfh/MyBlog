2a6f4d9a9306581746c3475f2651307a53a1b396
changxin <ccxysfh1993@gmail.com>
2017-04-04 17:24:18 +0800

    add cs cards models

--- computer_science_flash_cards/models.py
+++ computer_science_flash_cards/models.py
@@ -1,16 +1,16 @@
 from django.db import models,connection,connections
 
 
-	
+
 # Create your models here.
-class Cards(object):
+class Cards(models.Model):
 	"""docstring for Cards"""
 	id=models. AutoField(primary_key=True)
-	type=models.IntegerField(null=False,blank=False) #1 for vocab, 2 for code 
+	type=models.IntegerField(null=False,blank=False) #1 for vocab, 2 for code
 	front=models.TextField(null=False,blank=False)
 	back=models.TextField(null=False,blank=False)
 	known=models.BooleanField(default=False)
- 
+
 class BaseManage(models.Manager):
 
 	def get_db(self):

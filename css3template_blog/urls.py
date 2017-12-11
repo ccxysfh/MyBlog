a55e7107691c00e63dd92eb62d96dc7aef25f417
changxincheng <chengcx1019@gmail.com>
2017-12-12 01:55:21 +0800

    url

--- css3template_blog/urls.py
+++ css3template_blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url('^archive/$', views.archive),
     url('^shares/$', views.shares),
     url('^profile/$',views.profile),
+    url('^happy_birthday_to_my_princess_huyao/$',views.happy_birthday),
 ]

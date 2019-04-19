4bb657c2136c051b0b125b14fd2a4e5e96bbe7ca
ChengCX <chengcx1019@gmail.com>
2019-04-19 19:15:23 +0800

    2 vue (#8)
    * fix: change static dir
    
    * feat: rss
    
    * feat: init vue src
    
    * fix: archive float
    
    * feat: use django 2.1.7
    
    * feat: blog front
    
    * feat: combine vue to django
    
    * feat: combine  vue url to bankend
    
    * fix: conflict

--- urls.py
+++ urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.urls import re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
     # admin
-    path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     # blog_api
     path(r'blog/', include('blog_api.urls')),
+    path(r'', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
     # computer_science_flash_cards
     # path(r'flash/', include('computer_science_flash_cards.urls')),
 

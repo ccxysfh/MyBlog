97765f09a07af787edbc27e4ccbce30077defedf
Changxin-Cheng <changxin.cheng@nio.com>
2020-04-23 23:00:01 +0800

    feat: 每日备份sql 4次

--- config/cronjob.py
+++ config/cronjob.py
@@ -7,12 +7,19 @@
 @time: 2019-05-14 14:25
 """
 
+import os
+from time import strftime
+
 import requests
+from django.conf import settings
 from lxml import html
 
 from blog_api.models import BlogPost
-from blog_api.views import get_remote_source, save_blogpost,remove_when_update
+from blog_api.views import get_remote_source, save_blogpost, remove_when_update
 from config.settings import logger
+
+PROJECT_DIR = settings.BASE_DIR
+LOG_PATH = os.path.join(PROJECT_DIR, 'sql')
 
 ext = ".md"
 
@@ -60,5 +67,14 @@ def trigger_update_blog():
                 logger.warn("blogpost:{id} update from {remote_source} fail,error detail:{err}"
                             .format(id=blogpost.id, remote_source=remote_source, err=e))
 
+
+def backup_sql():
+    cmd_pattern = 'mysqldump -ublogcx -p789012 blogcx > {dir}/blogcx{time_str}.sql'
+    dir_name = os.path.join(LOG_PATH, strftime('%Y%m'))
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+    cmd = cmd_pattern.format(dir=dir_name, time_str=strftime('%Y%m%d%H'))
+    os.system(cmd)
+
 if __name__ == '__main__':
-    trigger_update_blog()
+    backup_sql()

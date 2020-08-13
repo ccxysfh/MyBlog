6eb62b6df30647afb0f17a5629b52ba206993998
Changxin-Cheng <changxin.cheng@nio.com>
2020-08-13 14:04:14 +0800

    feat: admin page

--- stock_record/admin.py
+++ stock_record/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import StockRecord
+
+
+class StockRecordAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(StockRecord, StockRecordAdmin)

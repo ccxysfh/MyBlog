181c0dbe2a585425cc86dd358dc9317439207827
changxin-cheng <chengcx1019@gmail.com>
2020-08-16 02:03:57 +0800

    fix: amin fields

--- stock_record/admin.py
+++ stock_record/admin.py
@@ -8,8 +8,8 @@ from .models import StockRecord
 class StockRecordAdmin(admin.ModelAdmin):
     list_display = ('stock_code','stock_name','status',
                     'type','in_price','in_number',
-                    'out_price','out_number','excepted_in_price',
-                    'excepted_in_number','description','show','focus_on','pub_date','last_edit_date'
+                    'out_price','out_number','excepted_next_price',
+                    'excepted_next_number','description','show','focus_on','pub_date','last_edit_date'
                     )
     list_filter = ('stock_code','status','type','focus_on','show')
 
